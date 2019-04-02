7887ab16c34785c949668527477f959e98296cab
bellasherring <47996769+bellasherring@users.noreply.github.com>
2019-04-02 18:28:11 +1100

    create decoding.c
    fairly basic framework kinda

---
+++ decoding.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+//decoding function prototype
+char decrypt(char *phrase);
+
+int main()
+{
+    return 0;
+}
+
+//decoding function definition
+char decrypt(char *phrase)
+{
+    //function body
+}
